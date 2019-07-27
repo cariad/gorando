f69e2d1d6c679c0e98f7860c8e6eec02443f6486
Cariad Eccleston <cariad@cariad.me>
2019-07-27 14:00:35 +0100

    Add alphanumeric constants

---
+++ constants.go
@@ -0,0 +1,12 @@
+package gorando
+
+// LowerAlphabet is a string containing all the characters in the English
+// alphabet in lower-case form.
+const LowerAlphabet = "abcdefghijklmnopqrstuvwxyz"
+
+// UpperAlphabet is a string containing all the characters in the English
+// alphabet in lower-case form.
+const UpperAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+// Digit is a string containing all the base-10 digits.
+const Digit = "0123456789"
